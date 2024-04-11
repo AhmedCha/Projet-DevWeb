eb6dbd834ea0e1d19150afd05c67e8673f8eebc0
Ahmed Chaabane <ahmedcha235.cha@gmail.com>
2024-04-11 18:06:03 +0100

    modified:   CSS/indexStyle.css 	modified:   CSS/loginStyle.css 	deleted:    CSS/proposStyle.css 	modified:   CSS/signupStyle.css 	modified:   JavaScript/indexScript.js 	modified:   index.html 	modified:   login.html 	deleted:    propos.html 	modified:   signup.html 	new file:   src/26047.jpg 	new file:   src/26047.png 	new file:   src/bullet-point.png

--- JavaScript/indexScript.js
+++ JavaScript/indexScript.js
@@ -1,3 +1,17 @@
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offsetTop = targetElement.offsetTop - 120; // Subtracting 100px
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
 document.getElementById("login").onclick = function () {
     location.href = "login.html";
 };
